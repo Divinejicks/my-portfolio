1105e74533bf5aa6507b2dc4efaaf7eec1e6fa85
Mbuh Divine <divinejicks@gmail.com>
2025-02-17 19:00:09 +0100

    whatsapp

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { HomePage } from './components/home'
+import WhatsAppButton from './components/whatsappButton'
 
 function App() {
 
   return (
     <>
+      <WhatsAppButton />
       <HomePage />
     </>
   )
